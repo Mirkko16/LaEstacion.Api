b777697918f014315037b8999034895193120495
Mirkko16 <mirkko1996@hotmail.com>
2024-01-04 17:29:21 -0300

    cambios sobre dll

--- Repository/Productos/StockController.cs
+++ Repository/Productos/StockController.cs
@@ -17,31 +17,31 @@ namespace LaEstacion.Repository.Productos
             _pdfService = pdfService;
         }
 
-        public async Task VerificarStockMinimoYGenerarPdf()
-        {
-            try
-            {
-                // Realiza la verificación de stock mínimo
-                await _productoRepository.VerificarStockMinimo();
+        //public async Task VerificarStockMinimoYGenerarPdf()
+        //{
+        //    try
+        //    {
+        //        // Realiza la verificación de stock mínimo
+        //        await _productoRepository.VerificarStockMinimo();
 
-                // Obtén la lista de productos que están por debajo del stock mínimo
-                var productosBajoStock = await _productoRepository.ObtenerProductosBajoStockMinimo();
+        //        // Obtén la lista de productos que están por debajo del stock mínimo
+        //        var productosBajoStock = await _productoRepository.ObtenerProductosBajoStockMinimo();
 
-                // Genera el PDF con la lista de productos
-                var pdfBytes = _pdfService.GenerarPdf(productosBajoStock);
+        //        // Genera el PDF con la lista de productos
+        //        var pdfBytes = _pdfService.GenerarPdf(productosBajoStock);
 
-                // Puedes guardar el PDF en el sistema de archivos, enviarlo por correo, etc.
-                // Por ejemplo, guardarlo en la carpeta "ArchivosGenerados"
-                var rutaPdf = $"ArchivosGenerados/ProductosBajoStock_{DateTime.Now:yyyyMMddHHmmss}.pdf";
-                System.IO.File.WriteAllBytes(rutaPdf, pdfBytes);
+        //        // Puedes guardar el PDF en el sistema de archivos, enviarlo por correo, etc.
+        //        // Por ejemplo, guardarlo en la carpeta "ArchivosGenerados"
+        //        var rutaPdf = $"ArchivosGenerados/ProductosBajoStock_{DateTime.Now:yyyyMMddHHmmss}.pdf";
+        //        System.IO.File.WriteAllBytes(rutaPdf, pdfBytes);
 
-                // Puedes realizar otras acciones según tus necesidades
-            }
-            catch (Exception ex)
-            {
-                // Manejo de errores
-                Console.WriteLine($"Error al verificar stock mínimo y generar PDF: {ex.Message}");
-            }
-        }
+        //        // Puedes realizar otras acciones según tus necesidades
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Manejo de errores
+        //        Console.WriteLine($"Error al verificar stock mínimo y generar PDF: {ex.Message}");
+        //    }
+        //}
     }
 }
