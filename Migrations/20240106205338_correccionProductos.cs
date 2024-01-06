8b43426f61c9ff78bc8be3ea1e30cfef29f62687
Mirkko16 <mirkko1996@hotmail.com>
2024-01-06 17:54:36 -0300

    se creo el campo stock minimo y se hizo migracion

---
+++ Migrations/20240106205338_correccionProductos.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LaEstacion.Migrations
+{
+    public partial class correccionProductos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserLogIn",
+                table: "Usuarios",
+                newName: "Username");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "StockMinimo",
+                table: "Productos",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StockMinimo",
+                table: "Productos");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Usuarios",
+                newName: "UserLogIn");
+        }
+    }
+}
